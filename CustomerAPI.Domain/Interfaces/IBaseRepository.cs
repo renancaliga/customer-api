b41c190d3aebb5878a4ac84f473b6f23495d3c0b
rcaliga <renan.caliga@branef.com.br>
2023-04-17 21:14:25 -0300

    Adicionar arquivos de projeto.

---
+++ CustomerAPI.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using CustomerAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Domain.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity 
+        : BaseEntity
+    {
+        void Insert(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(TEntity obj);
+        IList<TEntity> Select();
+        TEntity Select(int id);
+    }
+}
