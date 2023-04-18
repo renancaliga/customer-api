80bbe1c1dd340262a694f19d091e9f36fdd70e17
rcaliga <renan.caliga@branef.com.br>
2023-04-17 23:18:27 -0300

    ajustes

---
+++ CustomerAPI.Application/Startup.cs
@@ -0,0 +1,47 @@
+﻿using CustomerAPI.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAPI.Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors();
+            services.AddControllers();         
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddDbContext<SqlContext>(
+                opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+
+            var url = "http://localhost:8080/";
+
+
+            app.UseCors(b => b.WithOrigins(url));
+
+            if (app.Environment.IsDevelopment())
+            {
+
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
--- CustomerAPI.Infra.Data/Context/SqlContext.cs
+++ CustomerAPI.Infra.Data/Context/SqlContext.cs
@@ -5,17 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CustomerAPI.Domain.Entities;
+using CustomerAPI.Infra.Data.Mapping;
 
 namespace CustomerAPI.Infra.Data.Context
 {
     public class SqlContext : DbContext
     {
+        public SqlContext() : base()
+        {
+
+        }
         public SqlContext(DbContextOptions<SqlContext> options) 
             : base(options)
         {
             
         }
-
+      
         public DbSet<Customer> Customers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
